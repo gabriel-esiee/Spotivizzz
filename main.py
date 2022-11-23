815a3ed8edbc77d7c4d078a0af317f4f84a7d6f5
Ckyre <ckyre.game@gmail.com>
2022-11-23 16:54:22 +0100

    Project file structure

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Run this app with `python main.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-import fetch
-import dashboard
+from src import fetch
+from src import dashboard
 
 datas = fetch.fetch_data()
 dashboard.build_main_layout(datas)
