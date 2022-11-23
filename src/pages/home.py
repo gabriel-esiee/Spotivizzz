3030c4a7bd67623bef4d43b7e3cb91a1e70e6695
Ckyre <ckyre.game@gmail.com>
2022-11-23 18:42:45 +0100

    Created home page template

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,8 +1,36 @@
 from dash import html
 
 home = html.Div(
-    [
-        html.H2(children="Hello Dash!")
-    ],
-    className="large-container"
+    className="home large-container",
+    children=[
+        html.Section(
+            children=[
+                html.H2(
+                    children="Your datas"
+                ),
+                html.Div(
+                    className="graph-container"
+                ),
+            ]
+        ),
+        
+        html.Section(
+            children=[
+                html.H2(
+                    children="World dashboard"
+                ),
+                html.Div(
+                    className="graph-grid",
+                    children=[
+                        html.Div(className="graph-container world-map"),
+                        html.Div(className="graph-container"),
+                        html.Div(className="graph-container"),
+                        html.Div(className="graph-container"),
+                        html.Div(className="graph-container")
+                    ]
+                ),
+            ]
+        ),
+
+    ]
 )
